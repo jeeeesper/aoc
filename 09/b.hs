aa194b50feb27f09dc86d12731d4f8da6c3524de
Jasper Krauter <jasper.krauter@uni-konstanz.de>
2020-12-09 13:29:48 +0100

    solution to day 9

---
+++ 09/b.hs
@@ -0,0 +1,16 @@
+import Data.List
+
+main :: IO ()
+main = interact $ show . solve . reverse . map read . lines
+
+preamble :: Int
+preamble = 25
+
+solve :: [Integer] -> Integer
+solve xs = minimum ns + maximum ns
+  where
+    ns = head . filter ((==) n . sum) . concatMap tails $ inits ns'
+    (n:ns') = solve' xs
+    solve' [] = error "fookin laser soights"
+    solve' ys@(y:ys') | null [a+b | a <- take preamble ys', b <- take preamble ys', a+b==y] = ys
+                      | otherwise = solve' ys'
