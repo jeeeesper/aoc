ff36f8a4eec919d3090fd1c22298bb501384a136
Jasper Krauter <jasper.krauter@uni-konstanz.de>
2020-12-11 21:35:42 +0100

    solution to day 10

---
+++ 10/a.hs
@@ -0,0 +1,13 @@
+import Data.List
+
+main :: IO ()
+main = interact $ show . solve . sort . map read . lines
+
+solve :: [Integer] -> Integer
+solve = go (0,0) . (:) 0
+  where
+    go _ [] = error "empty input"
+    go (o,t) [_] = o * (t+1)
+    go (o,t) (x:xs@(x':_)) | x'-x == 1 = go (o+1,t) xs
+                           | x'-x == 3 = go (o,t+1) xs
+                           | otherwise = error "ich raste aus"
