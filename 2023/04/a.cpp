e45f7e57f684b932b086972be3dc5935cd29b07e
Jasper Krauter <12259417+DrKongoIV@users.noreply.github.com>
2023-12-05 15:05:53 +0100

    2023 day 4

---
+++ 2023/04/a.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+#include "../common.cpp"
+
+int main() {
+  unsigned int sum;
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    auto split = util::split_string(util::split_string(line, ':')[1], '|');
+    auto winning = util::split_string(split[0], ' ');
+    int score = 0;
+    for (auto num : util::split_string(split[1], ' ')) {
+      if (std::find(winning.begin(), winning.end(), num) != winning.end()) {
+        score = (score == 0) ? 1 : score * 2;
+      }
+    }
+    sum += score;
+  }
+  std::cout << sum << std::endl;
+}
---
+++ 2023/04/b.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <numeric>
+
+#include "../common.cpp"
+
+int main() {
+  std::vector<int> matches;
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    auto split = util::split_string(util::split_string(line, ':')[1], '|');
+    auto winning = util::split_string(split[0], ' ');
+    int score = 0;
+    for (auto num : util::split_string(split[1], ' ')) {
+      if (std::find(winning.begin(), winning.end(), num) != winning.end()) {
+        ++score;
+      }
+    }
+    matches.push_back(score);
+  }
+
+  std::vector<int> card_count(matches.size(), 1);
+  for (size_t i = 0; i < matches.size(); ++i) {
+    for (size_t j = i + 1; j < i + 1 + matches[i]; ++j) {
+        card_count[j] += card_count[i];
+    }
+  }
+
+  std::cout << std::reduce(card_count.begin(), card_count.end()) << std::endl;
+}
