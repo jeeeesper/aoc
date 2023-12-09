2897c198d2eaba920d09022057ed8f1772508929
Jasper Krauter <12259417+DrKongoIV@users.noreply.github.com>
2023-12-09 13:17:09 +0100

    2023 day 9

---
+++ 2023/09/b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+#include "../common.h"
+
+int extrapolate(std::vector<int>& nums) {
+  if (std::all_of(nums.begin(), nums.end(), [](int n) { return n == 0; })) return 0;
+  int last = nums[nums.size() - 1];
+  for (size_t i = 0; i < nums.size() - 1; ++i) {
+    nums[i] = nums[i + 1] - nums[i];
+  }
+  nums.pop_back();
+  return last + extrapolate(nums);
+}
+
+int main() {
+  std::string line;
+  int sum = 0;
+  while (std::getline(std::cin, line)) {
+    std::vector<int> nums;
+    for (auto& n : util::split_string(line, ' ')) nums.push_back(std::stoi(n));
+    std::reverse(nums.begin(), nums.end());
+    sum += extrapolate(nums);
+  }
+  std::cout << sum << std::endl;
+}
